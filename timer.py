94277efc4f83c54beeb611b56ee19dd4aa053b82
AnuoluwapoDS <83256563+Designegycreatives@users.noreply.github.com>
2022-07-01 00:12:21 +0100

    Update timer.py

--- timer.py
+++ timer.py
@@ -1,18 +1,16 @@
 import streamlit as st
 import base64
 import time
+from PIL import Image
 
-st.header("Count Down Timer App")
+image = Image.open('image.png')
 
-file_ = open("image.gif", "rb")
-contents = file_.read()
-data_url = base64.b64encode(contents).decode("utf-8")
-file_.close()
+col1, col2 = st.columns(2)
 
-st.markdown(
-    f'<img src="data:image/gif;base64,{data_url}" alt="dashboard gif">',
-     unsafe_allow_html=True
-)
+col1.header("Count Down Timer App")
+col2.image(image)
+
+
 
 
 def countdown(t):
